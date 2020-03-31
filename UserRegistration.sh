fd02d76fdaf761faac9d12049c8efa9db1a3eff1
root <root@localhost.localdomain>
2020-03-31 22:35:26 +0000

    Validate Email Address

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,7 +6,7 @@
 read -p "Enter First Name:" FIRSTNAME
 read -p "Enter Last Name:" LASTNAME
 
-PATTERN=^[A-Z]{1}[a-zA-Z]{2,}
+PATTERN="^[A-Z]{1}[a-zA-Z]{2,}"
 if [[ $FIRSTNAME =~ $PATTERN ]] && [[ $LASTNAME =~ $PATTERN ]]
 then
 	echo "Name is valid"
@@ -14,4 +14,14 @@ else
 	echo "Name is invalid"
 fi
 
+#Use case 3
 
+read -p "Enter Email Address:" EMAIL
+
+PATTERN="^[0-9a-z]+[+_.-]?[0-9a-z]+[@][a-z]+[.]{1}[a-z]{2,}[.]?[a-z]+$"
+if [[ $EMAIL =~ $PATTERN ]]
+then
+	echo "Email is valid"
+else
+	echo "Email is invalid"
+fi
