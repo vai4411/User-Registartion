d08da816043775a4d591064dcf1ec3b6f88f362c
root <root@localhost.localdomain>
2020-03-31 21:42:04 +0000

    Validate Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 
 #Use case 1
+#Use case 2
+
 read -p "Enter First Name:" FIRSTNAME
+read -p "Enter Last Name:" LASTNAME
 
 PATTERN=^[A-Z]{1}[a-zA-Z]{2,}
-if [[ $FIRSTNAME =~ $PATTERN ]]
+if [[ $FIRSTNAME =~ $PATTERN ]] && [[ $LASTNAME =~ $PATTERN ]]
 then
-	echo "First Name is valid"
+	echo "Name is valid"
 else
-	echo "First Name is invalid"
+	echo "Name is invalid"
 fi
+
+
